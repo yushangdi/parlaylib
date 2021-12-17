a80e2941fc4856fb903767f8f983d00e500b7101
Daniel Anderson <dlanders@cs.cmu.edu>
2021-12-16 22:43:13 -0500

    Try lazily initializing the static allocator

--- include/parlay/alloc.h
+++ include/parlay/alloc.h
@@ -12,21 +12,25 @@ class type_allocator {
 public:
   static constexpr inline size_t default_alloc_size = 0;
   static constexpr inline bool initialized = true;
-  static inline block_allocator allocator = block_allocator(sizeof(T));
 
-  static T* alloc() { return static_cast<T*>(allocator.alloc()); }
-  static void free(T* ptr) { allocator.free(static_cast<void*>(ptr)); }
+  static block_allocator& get_allocator() {
+    static block_allocator allocator(sizeof(T));
+    return allocator;
+  }
+
+  static T* alloc() { return static_cast<T*>(get_allocator().alloc()); }
+  static void free(T* ptr) { get_allocator().free(static_cast<void*>(ptr)); }
 
   // for backward compatibility
   static void init(size_t, size_t) {};
   static void init() {};
-  static void reserve(size_t n = default_alloc_size) { allocator.reserve(n); }
-  static void finish() { allocator.clear(); }
-  static size_t block_size () { return allocator.block_size(); }
-  static size_t num_allocated_blocks() { return allocator.num_allocated_blocks(); }
-  static size_t num_used_blocks() { return allocator.num_used_blocks(); }
+  static void reserve(size_t n = default_alloc_size) { get_allocator().reserve(n); }
+  static void finish() { get_allocator().clear(); }
+  static size_t block_size () { return get_allocator().block_size(); }
+  static size_t num_allocated_blocks() { return get_allocator().num_allocated_blocks(); }
+  static size_t num_used_blocks() { return get_allocator().num_used_blocks(); }
   static size_t num_used_bytes() { return num_used_blocks() * block_size(); }
-  static void print_stats() { allocator.print_stats(); }
+  static void print_stats() { get_allocator().print_stats(); }
 };
 
 
